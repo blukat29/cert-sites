1ae8f3313d5e78b13dffd98c9c7ec0c4aeafa248
blukat29 <yunjong@kaist.ac.kr>
2014-10-15 18:02:56 +0900

    Add a login_required view /report/

--- cert/cert/urls.py
+++ cert/cert/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^report/', include('report.urls')),
 )
