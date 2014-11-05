321a6d6b629977a946a61a5d52b12de59568b37b
blukat29 <yunjong@kaist.ac.kr>
2014-11-05 17:50:55 +0900

    Extend UserCreationForm to RegistrationForm

--- cert/cert/urls.py
+++ cert/cert/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
+from forms import RegistrationForm
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'cert.views.home', name='home'),
@@ -14,6 +16,6 @@ urlpatterns = patterns('',
     url(r'^report/', include('report.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^register/', CreateView.as_view(template_name="register.html",
-                                          form_class=UserCreationForm,
+                                          form_class=RegistrationForm,
                                           success_url="/")),
 )
