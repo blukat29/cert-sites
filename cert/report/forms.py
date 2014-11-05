2100333277e56eeb3403067a958839b3ec034b94
blukat29 <yunjong@kaist.ac.kr>
2014-11-06 00:15:56 +0900

    Set widgets for ReportForm

--- cert/report/forms.py
+++ cert/report/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.forms.extras.widgets import SelectDateWidget
+
+from datetime import datetime
 
 from report.models import Report
 
 class ReportForm(ModelForm):
+    text = forms.CharField(widget=forms.Textarea)
+    date = forms.DateField(initial=datetime.now)
     class Meta:
         model = Report
         fields = ["url", "text", "date"]
--- cert/report/models.py
+++ cert/report/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from datetime import datetime
-
 class Report(models.Model):
     user = models.ForeignKey(User)
     url = models.CharField(max_length=300)
