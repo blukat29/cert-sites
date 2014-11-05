f491cf80b006275920ef48d1d25e04d845cf2810
blukat29 <yunjong@kaist.ac.kr>
2014-11-06 04:55:23 +0900

    Edit report at /report/edit/<report_id>/

--- cert/report/urls.py
+++ cert/report/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
 
 from report import views
-from report.views import ReportCreateView
+from report.views import ReportCreateView, ReportUpdateView
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name="report_index"),
     url(r'^create/$', login_required(ReportCreateView.as_view()), name="report_create"),
     url(r'read/(?P<report_id>\d+)/$', views.read, name="report_read"),
+    url(r'edit/(?P<report_id>\d+)/$', login_required(ReportUpdateView.as_view()), name="report_update"),
 )
--- cert/report/views.py
+++ cert/report/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User
 
 from report.models import Report
@@ -35,5 +35,31 @@ class ReportCreateView(CreateView):
         context = super(ReportCreateView, self).get_context_data(**kwargs)
         context["user"] = self.request.user
         context["reporter"] = self.request.user
+        context["valid_report"] = True
         return context
+
+class ReportUpdateView(UpdateView):
+    model = Report
+    form_class = ReportForm
+    template_name = "report/create.html"
+
+    def get_object(self):
+        try:
+            report = Report.objects.get(id=self.kwargs["report_id"])
+        except Report.DoesNotExist:
+            report = None
+        return report
+
+    def get_context_data(self, **kwargs):
+        context = super(ReportUpdateView, self).get_context_data(**kwargs)
+        context["user"] = self.request.user
+        if self.object:
+            context["reporter"] = self.object.user
+            context["valid_report"] = True
+        else:
+            context["valid_report"] = False
+        return context
+
+    def get_success_url(self):
+        return "/report/read/" + str(self.kwargs["report_id"])
 
