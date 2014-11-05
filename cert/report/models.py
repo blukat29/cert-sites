92355b8ca92dc9b72ddacd810b4285954003a112
blukat29 <yunjong@kaist.ac.kr>
2014-11-05 18:09:43 +0900

    Create first Report model

--- cert/report/models.py
+++ cert/report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+class Report(models.Model):
+    user = models.OneToOneField(User)
+    url = models.CharField(max_length=300)
+    text = models.CharField(max_length=10000)
+    date = models.DateField()
+    # TODO tags
+    # TODO images
